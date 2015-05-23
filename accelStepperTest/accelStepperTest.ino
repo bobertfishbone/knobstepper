336800d235570c5469f300933c25fcb597a14a0d
bobertfishbone <bobertfishbone@gmail.com>
2015-05-23 11:32:45 -0400

    First Functional AccelStepper

--- accelStepperTest/accelStepperTest.ino
+++ accelStepperTest/accelStepperTest.ino
@@ -7,7 +7,7 @@
 #include <Adafruit_MotorShield.h>
 
 // Time variables
-long duration = 10000; // 10 seconds in milliseconds
+long duration = 20000; // 10 seconds in milliseconds
 long millistart; // Grabs milliseconds since power on once motor has initially stepped.
 bool run = false; // Has the cycle ended?
 
@@ -17,12 +17,12 @@ long oldPosition = 0; // Encoder reference point
 
 // Create motor, encoder objects
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Encoder enc(2,3);
-Adafruit_StepperMotor *myMotor = AFMS.getStepper(200,1);
-
+Encoder enc(2, 5);
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 1);
+AccelStepper stepper(forwardStep, backwardStep);
 void forwardStep()
 {
- myMotor->onestep(FORWARD, MICROSTEP); 
+  myMotor->onestep(FORWARD, MICROSTEP);
 }
 
 void backwardStep()
@@ -30,52 +30,62 @@ void backwardStep()
   myMotor->onestep(BACKWARD, MICROSTEP);
 }
 
-AccelStepper stepper(forwardStep, backwardStep);
+
 
 
 
 
 void setup() {
   // put your setup code here, to run once:
-  stepper.setCurrentPosition(0);
-  stepper.setSpeed(4);
-  
-  stepper.moveTo(numberofSteps);
+  AFMS.begin();
+
+  stepper.setMaxSpeed(100);
+  stepper.setAcceleration(500);
+  //stepper.setCurrentPosition(0);
+  stepper.runToNewPosition(1200);
   
   millistart = millis();
 }
 
 void loop() {
+ if (run == false){
   // put your main code here, to run repeatedly:
+  stepper.setMaxSpeed(100);
+  stepper.setAcceleration(500);
+  long newPosition = enc.read();
 
-  long newPosition = enc.read()/4;
-  
+
+
   if (newPosition != oldPosition)
   {
-  if (newPosition > oldPosition)
-  {
-    stepper.moveTo(stepper.currentPosition()+(newPosition - oldPosition));
+    if (newPosition > oldPosition)
+    {
+      stepper.moveTo(stepper.currentPosition() + 40*(newPosition - oldPosition));
+      
+    }
+    else
+    {
+      stepper.moveTo(stepper.currentPosition() - 40*(oldPosition - newPosition));
+    }
   }
-  else
-  {
-   stepper.moveTo(stepper.currentPosition()-(oldPosition-newPosition)); 
-  }
-  }
-  
-  newPosition = enc.read()/4;
-  oldPosition = newPosition;
 
-if (millis() > millistart + duration)
+
+  oldPosition = newPosition;
+  stepper.run();
+  
+  if (millis() > millistart + duration)
   {
     if (run == false)
     {
-      
-      stepper.setSpeed(4);
-      stepper.moveTo(0);
-      run = true;
-    }
-    
-  }
 
+      stepper.setMaxSpeed(100);
+      stepper.setAcceleration(500);
+stepper.runToNewPosition(0);
+      run = true;
+      myMotor->release();
+    }
+
+  }
+ }
 
 }
